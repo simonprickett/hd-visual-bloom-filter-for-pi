76d9a4a41c624d27f53cd9d0b7ddebc1f157d325
Simon Prickett <simon@crudworks.org>
2021-09-12 18:37:07 +0100

    Demo start point code to check it's working.

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-# TODO
+import unicornhathd
+
+unicornhathd.rotation(0)
+unicornhathd.brightness(0.6)
+u_width, u_height = unicornhathd.get_shape()
+
+try:
+    for y in range(u_height):
+        for x in range(u_width):
+            unicornhathd.set_pixel(x, y, 255, 0, 0)
+    unicornhathd.show()
+
+    while True:
+        continue
+
+except KeyboardInterrupt:
+    unicornhathd.off()

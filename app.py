7f9036d1258e68b8efd1785f899a9fdd2e453d83
Simon Prickett <simon@crudworks.org>
2021-09-12 22:03:00 +0100

    Added more preamble.

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
+from flask import Flask
+from flask import jsonify
+from flask import render_template
+import mmh3
+import time
 import unicornhathd
+
+API_PATH_PREFIX = 'api'
+NUM_HASH_FUNCTIONS = 6
+COLOR_BIT_SET = (255, 0, 0)
+COLOR_BIT_WRITING = (0, 255, 0)
+COLOR_BIT_QUERYING = (0, 0, 255) 
+NUM_TRANSITIONS = 4
 
 unicornhathd.rotation(90)
 unicornhathd.brightness(0.6)
-u_width, u_height = unicornhathd.get_shape()
+unicornhd_width, unicornhd_height = unicornhathd.get_shape()
+unicornhathd.off()
+
+NUM_LEDS = unicornhd_width * unicornhd_height
 
 try:
     #for y in range(u_height):

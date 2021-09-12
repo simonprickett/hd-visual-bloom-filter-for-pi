8f2b21ea30b2be521c27e227c8ee3ffd9694f8d8
Simon Prickett <simon@crudworks.org>
2021-09-12 20:09:49 +0100

    Fixed orientation of UnicornHatHD.

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import unicornhathd
 
-unicornhathd.rotation(0)
+unicornhathd.rotation(90)
 unicornhathd.brightness(0.6)
 u_width, u_height = unicornhathd.get_shape()
 
 try:
-    for y in range(u_height):
-        for x in range(u_width):
-            unicornhathd.set_pixel(x, y, 255, 0, 0)
+    #for y in range(u_height):
+    #    for x in range(u_width):
+    #        unicornhathd.set_pixel(x, y, 255, 0, 0)
+
+    unicornhathd.set_pixel(0, 0, 255, 0, 0)
+    unicornhathd.set_pixel(15, 15, 0, 0, 255)
     unicornhathd.show()
 
     while True:
